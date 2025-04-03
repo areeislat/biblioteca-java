afbbc7b0851a43a6e137c880403d815d2761bb40
Areliz <are.isla@duocuc.cl>
2025-04-02 21:05:07 -0300

    se crea clase en repository

---
+++ src/main/java/com/example/biblioteca/repository/LibroRepository.java
@@ -0,0 +1,21 @@
+package com.example.biblioteca.repository;
+
+//se importan librerias
+import com.example.biblioteca.model.LibroModel;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository //como notacion, aprovecha la base del lenguaje java
+public class LibroRepository {
+    
+    private List<LibroModel> listaLibros = new ArrayList<>();
+
+    @PostConstruct
+    public void init() {
+        listaLibros.add(new LibroModel(2, "9999-999","Spring in action", 1, 1997, "Manning"));
+
+    }
+
+}
